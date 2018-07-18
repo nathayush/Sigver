eb7d863886da033a316f8ae76f989a20136a49e1
Ayush Nath <nathayush@gmail.com>
2018-07-18 14:26:18 +0530

    forward propagator + phase 2

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import trainer, dataset, shuffler
 from sklearn import preprocessing
+from sklearn import svm
+
 
 def main():
     num_classes = 21
+
+    ### PHASE 1
+
     name_encoder = preprocessing.LabelBinarizer()
 
     shuffler.shuffleData() # shuffle dataset
@@ -12,6 +17,17 @@ def main():
     mytrainer = trainer.Trainer(trainData, testData, num_users=num_classes) # number of people in the training set
     print("training.")
     mytrainer.train(num_epochs=60)
+
+    exit()
+
+    ### PHASE 2
+
+    # new_dataset for a user
+    # features = mytrainer.model.get_feature_vectors(new_dataset)
+    forg_classifier = svm.svc(class_weight='balanced')
+    forg_classifier_rbf = svm.svc(kernel='rbf', class_weight='balanced')
+    # forg_classifier.fit(features, labels)
+    # forg_classifier.predict(...)
 
 if __name__ == '__main__':
     main()
--- model.py
+++ model.py
@@ -5,6 +5,7 @@ class Signet(nn.Module):
     def __init__(self, num_users=10):
         super(Signet, self).__init__()
         self.num_classes = num_users
+        self.forward_util_layer = {}
 
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=96, kernel_size=11, stride=4)
         self.norm1 = nn.BatchNorm2d(96)
@@ -44,27 +45,30 @@ class Signet(nn.Module):
         self.fc_f = nn.Linear(in_features=2048, out_features=1)
         self.act_f = nn.Sigmoid()
 
+        self.features1, self.features2 = nn.Sequential(*list(self.children())[:-10]), nn.Sequential(*list(self.children())[18:-4])
+
     def forward(self, x):
         x = x.type(torch.FloatTensor)
         x = x.unsqueeze_(1)
 
-        x = self.act1(self.pool1(self.norm1(self.conv1(x))))
-
-        x = self.act2(self.pool2(self.norm2(self.conv2(x))))
-
-        x = self.act3(self.norm3(self.conv3(x)))
-
-        x = self.act4(self.norm4(self.conv4(x)))
-
-        x = self.act5(self.pool5(self.norm5(self.conv5(x))))
+        x = self.features1(x)
 
         x = x.view(-1, 256*3*5)
 
-        x = self.act6(self.norm6(self.fc6(x)))
-        x = self.act7(self.norm7(self.fc7(x)))
+        x = self.features2(x)
 
         m = self.act_m(self.fc_m(x))
         f = self.act_f(self.fc_f(x))
 
         # tuple of tensors
         return m, f
+
+    def get_feature_vectors(self, images):
+        x = images.type(torch.FloatTensor)
+        x = x.unsqueeze_(1)
+
+        x = self.features1(x)
+        x = x.view(-1, 256*3*5)
+        x = self.features2(x)
+
+        return x
